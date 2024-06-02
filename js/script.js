e72a58b427c2bf68ce45917ef1a890fcb61a080a
mateuscastronappe <teteucastro31@gmail.com>
2024-06-02 19:58:45 -0300

    Realizando a função do botão de entrar direcionando para o bem-vindo.html

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// document.getElementById("cadastroForm").addEventListener("submit", function(event) {
+//     event.preventDefault(); 
+//     window.location.href = "bem-vindo.html";
+// });
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("cadastroForm").addEventListener("submit", function(event) {
+        event.preventDefault(); 
+        window.location.href = "bem-vindo.html";
+    });
+});
